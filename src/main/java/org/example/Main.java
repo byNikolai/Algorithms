b2ef079efa7c3f4a34cdf8fe2780e7b8c17345b8
By_Nickolay <by.nickolay@gmail.com>
2023-06-20 22:16:16 +0300

    2.15

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import static org.example.IntegerListImpl.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int size = 100000;
+        long start = System.currentTimeMillis();
+        sortInsertion(new int[]{size});
+        System.out.println(System.currentTimeMillis() - start);
     }
 }
